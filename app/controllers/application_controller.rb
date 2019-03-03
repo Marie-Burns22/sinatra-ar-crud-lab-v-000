2d8edf498a44323a21637bdeb165090e3150ecc8
Marie Burns <mcburns2222@gmail.com>
2019-03-03 22:41:42 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,33 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do
+  get '/article/new' do
+    erb :new
   end
+  
+  post '/articles' do
+    erb :index
+  end
+  
+  get '/articles' do 
+    @articles = Article.all 
+    erb :index
+  end
+  
+  get '/aricles/:id' do 
+    @article = Article.find(params[:id])
+    erb :show 
+  end
+  
+  get '/articles/:id/edit' do 
+    erb :edit
+  end
+  
+  patch 'articles/:id' do
+  end
+  
+  delete '/articles/:id' do
+    erb :index
+  end
+
 end
